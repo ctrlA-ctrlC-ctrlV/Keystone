0f62f4c1cada58c00c9678e9a98ee60549f0de1f
ctrlA-ctrlC-ctrlV <tonybray86@gmail.com>
2025-09-19 13:38:52 +0100

    fix: themes.css not load

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./globals.css";
-import './theme.css'
+import './themes.css'
 import BootstrapClient from './bootstrap-client'
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
